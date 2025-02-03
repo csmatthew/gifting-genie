f4d16a661550af2739ed5777b5bb0ac4570c7601
csmatthew <christopher.s.matthew@gmail.com>
2025-02-03 12:22:50 +0000

    About Page

--- giftinggenie/urls.py
+++ giftinggenie/urls.py
@@ -23,6 +23,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
     path('accounts/', include('allauth.urls')),
     path('friends/', include('friendslist.urls')),
     path('myaccount/', include('myaccount.urls')),
